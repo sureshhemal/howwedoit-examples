c9ca385920b72b5be9a83929af185e4253f3983c
sureshhemal <sureshhemal123@gmail.com>
2025-07-17 01:12:42 +0530

    Binding Dependencies with Custom Parameters

---
+++ app/Sms/UseCases/Services/SmsServiceInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Sms\UseCases\Services;
+
+/**
+ * Interface for SMS service implementations.
+ */
+interface SmsServiceInterface
+{
+    /**
+     * Send an SMS message to the specified phone number.
+     *
+     * @param string $phone The recipient's phone number
+     * @param string $message The message content
+     * @return string The result of the operation
+     */
+    public function sendSms(string $phone, string $message): string;
+}
