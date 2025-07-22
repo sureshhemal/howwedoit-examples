4555c953bb257b5fd85fa03612e4136938a83370
sureshhemal <sureshhemal123@gmail.com>
2025-07-22 22:44:00 +0530

    Vue3 api comparison

---
+++ resources/js/pages/Composable/Partials/UserList.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { User } from '@/types';
+
+defineProps({
+    users: {
+        type: Array as () => User[],
+        required: true,
+    }
+})
+</script>
+
+<template>
+    <div v-for="user in users" :key="user.email">
+        {{user.name}} - {{user.email}}
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,11 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'verified'])->group(function () {
+   Route::get('/dashboard', fn () => Inertia::render('Dashboard'))->name('dashboard');
+
+   Route::get('/composable', fn () => Inertia::render('Composable/Index'))->name('composable');
+});
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
